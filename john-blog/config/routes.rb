aed97cafddb4c43494a4865c7717efd5c3028eac
John Talio <johntalio@yahoo.com>
2017-01-22 17:50:16 -0500

    Still working on routes

--- john-blog/config/routes.rb
+++ john-blog/config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  root 'posts#index'
+  resources :users
+
   get 'posts' => 'posts#index'
-  get 'posts' => 'posts#new'
+  get 'posts/new' => 'posts#new'
+  post 'posts' => 'posts#create'
   get 'posts/:id' => 'posts#show'
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 end
